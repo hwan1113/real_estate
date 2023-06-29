1e8639ec95750d29d4999f7f6905ab7727e2864f
Kyunghwan Kim <hwan@KyunghwcBookPro.lan>
2023-06-28 19:13:35 -0700

    update

--- SQL/sql_acs_excel.sql
+++ SQL/sql_acs_excel.sql
@@ -97,6 +97,8 @@
 -- where A._2013_SEX_AND_AGE__Total_population != 0 and A._2014_SEX_AND_AGE__Total_population != 0
 -- ) C order by _2019_2021 desc) D)
 
+
+
 -- rent_per_housing_price
 -- select A.Name, A._2013_SEX_AND_AGE__Total_population, A._2014_SEX_AND_AGE__Total_population, A._2015_SEX_AND_AGE__Total_population, A._2016_SEX_AND_AGE__Total_population,
 -- B._2017_SEX_AND_AGE__Total_population, B._2018_SEX_AND_AGE__Total_population, B._2019_SEX_AND_AGE__Total_population, B._2021_SEX_AND_AGE__Total_population
@@ -157,21 +159,59 @@
 -- )
 
 --income
-select name, state, county, tract, dp03_0062e as median_household_income from acs_5_economic_census_tract_fl_2021 aectf
-where county = '031'
+-- select A.Name, A.state, A.city, round(A.CP03_2013_062E *1.15) as _2013_median_household_income_adj, round(A.CP03_2014_062E *1.15) _2014_median_household_income_adj, round(A.CP03_2015_062E *1.15) _2015_median_household_income_adj, 
+-- round(A.CP03_2016_062E*1.15) _2016_median_household_income_adj, B.CP03_2017_062E _2017_median_household_income_adj, B.CP03_2018_062E _2018_median_household_income_adj, 
+-- B.CP03_2019_062E _2019_median_household_income_adj, B.CP03_2021_062E _2021_median_household_income_adj
+-- from acs_1_economic_city_2016 A
+-- join acs_1_economic_city_2021 B on A.name = B.name
 
-
-
-
+-- final
+--select A.name, A.state, A.county, A.tract, B.dp03_0062e as median_household_income_2016, A.dp03_0062e as median_household_income_2021, 
+--round(CAST((((A.dp03_0062e - B.dp03_0062e )/B.dp03_0062e )*100) as numeric), 4) as price_increase
+--from acs_5_economic_census_tract_fl_2021 A
+--join acs_5_economic_census_tract_fl_2016 B
+--on A.name = B.name
+--where A.county = '031' and B.dp03_0062e != 0
 
 
 -- Housing units
--- select B.Name, B.state, B.city, B._2013_Total_housing_units_x, B._2014_Total_housing_units_x, B._2015_Total_housing_units_x, B._2016_Total_housing_units_x,
--- A._2017_Total_housing_units_x, A._2018_Total_housing_units_x, A._2019_Total_housing_units_x, A._2021_Total_housing_units_x 
--- from acs_1_housing_city_2021 A
--- join acs_1_housing_city_2016 B on A.name = B.name
+-- select A.Name, A.state, A.city, A._2013_Total_housing_units_x, A._2014_Total_housing_units_x, A._2015_Total_housing_units_x, A._2016_Total_housing_units_x,
+-- B._2017_Total_housing_units_x, B._2018_Total_housing_units_x, B._2019_Total_housing_units_x, B._2021_Total_housing_units_x
+-- from acs_1_housing_city_2016 A
+-- join acs_1_housing_city_2021 B on A.name = B.name
+
+-- select A.Name, A.state, A.city, A._2013_Total_housing_units_x, A._2014_Total_housing_units_x, A._2015_Total_housing_units_x, A._2016_Total_housing_units_x,
+-- B._2017_Total_housing_units_x, B._2018_Total_housing_units_x, B._2019_Total_housing_units_x, B._2021_Total_housing_units_x
+-- from acs_1_housing_city_2016 A
+-- join acs_1_housing_city_2021 B on A.name = B.name
+-- where A._2013_Total_housing_units_x != 0
+
+-- final
+--select C.*,
+--round(CAST((((C._2014_Total_housing_units_x - C._2013_Total_housing_units_x)/C._2013_Total_housing_units_x)*100) as numeric), 4) as _2013_2014,
+-- round(CAST((((C._2015_Total_housing_units_x - C._2014_Total_housing_units_x)/C._2014_Total_housing_units_x)*100) as numeric), 4) as _2014_2015,
+-- round(CAST((((C._2016_Total_housing_units_x - C._2015_Total_housing_units_x)/C._2015_Total_housing_units_x)*100) as numeric), 4) as _2015_2016,
+-- round(CAST((((C._2017_Total_housing_units_x - C._2016_Total_housing_units_x)/C._2016_Total_housing_units_x)*100) as numeric), 4) as _2016_2017,
+-- round(CAST((((C._2018_Total_housing_units_x - C._2017_Total_housing_units_x)/C._2017_Total_housing_units_x)*100) as numeric), 4) as _2017_2018,
+-- round(CAST((((C._2019_Total_housing_units_x - C._2018_Total_housing_units_x)/C._2018_Total_housing_units_x)*100) as numeric), 4) as _2018_2019,
+-- round(CAST((((C._2021_Total_housing_units_x - C._2019_Total_housing_units_x)/C._2019_Total_housing_units_x)*100) as numeric), 4) as _2019_2021
+--from (
+-- select A.Name, A.state, A.city, A._2013_Total_housing_units_x, A._2014_Total_housing_units_x, A._2015_Total_housing_units_x, A._2016_Total_housing_units_x,
+-- B._2017_Total_housing_units_x, B._2018_Total_housing_units_x, B._2019_Total_housing_units_x, B._2021_Total_housing_units_x
+-- from acs_1_housing_city_2016 A
+-- join acs_1_housing_city_2021 B on A.name = B.name
+-- where A._2013_Total_housing_units_x != 0
+--) C
+ 
+-- census_tract
+--select A.name, A.state, A.county, A.tract, A."_total_housing_units_x" as total_housing_unit_2016, B."_total_housing_units_x" as total_housing_unit_2021 from 
+--acs_5_housing_census_tract_fl_2016 A join acs_5_housing_census_tract_fl_2021 B
+--on A.name = B.name
+
 
 -- Racial
+-- final
+--select * from (
 -- select A.Name, A.state, A.city, 'white' as race, A._2013_RACE__One_race__White as _2013, A._2014_RACE__One_race__White _2014, 
 -- A._2015_RACE__One_race__White _2015, A._2016_RACE__One_race__White _2016, B._2017_RACE__Total_population__One_race__White _2017,
 -- B._2018_RACE__Total_population__One_race__White _2018, B._2019_RACE__Total_population__One_race__White _2019,
@@ -200,20 +240,48 @@ where county = '031'
 -- B._2021_RACE__Total_population__One_race__Some_other_race _2021
 -- from acs_1_demographic_city_2016 A
 -- join acs_1_demographic_city_2021 B on A.name = B.name
+-- ) C
+--order by C.name, C.race
+
+--census_tract
+--select * from (
+-- select A.Name, A.state, A.county, A.tract, 'white' as race, A."_race__one_race__white"  as _2016, B."_race__total_population__one_race__white" as _2021
+-- from acs_5_demographic_census_tract_fl_2016 A
+-- join acs_5_demographic_census_tract_fl_2021 B on A.name = B.name
+-- union
+-- select A.Name, A.state, A.county, A.tract, 'black' as race, A."_race__one_race__black"  as _2016, B."_race__total_population__one_race__black" as _2021
+-- from acs_5_demographic_census_tract_fl_2016 A
+-- join acs_5_demographic_census_tract_fl_2021 B on A.name = B.name
+-- union
+--  select A.Name, A.state, A.county, A.tract, 'asian' as race, A."_race__one_race__asian"  as _2016, B."_race__total_population__one_race__asian" as _2021
+-- from acs_5_demographic_census_tract_fl_2016 A
+-- join acs_5_demographic_census_tract_fl_2021 B on A.name = B.name
+-- union
+-- select A.Name, A.state, A.county, A.tract, 'others' as race, A."_race__one_race__some_other_race" as _2016, B."_race__total_population__one_race__some_other_race" as _2021
+-- from acs_5_demographic_census_tract_fl_2016 A
+-- join acs_5_demographic_census_tract_fl_2021 B on A.name = B.name
+-- ) C
+-- order by C.name, C.race
+ 
 
 -- age
--- select A.Name, A.state, A.city, A._2013_SEX_AND_AGE__Median_age_y, A._2014_SEX_AND_AGE__Median_age_y, A._2015_SEX_AND_AGE__Median_age_y, A._2016_SEX_AND_AGE__Median_age_y,
--- B._2017_sex_and_age__total_population__median_age_, B._2018_sex_and_age__total_population__median_age_, B._2019_sex_and_age__total_population__median_age_,
--- B._2021_sex_and_age__total_population__median_age_
--- from acs_1_demographic_city_2016 A
--- join acs_1_demographic_city_2021 B on A.name = B.name
+ select A.Name, A.state, A.city, A._2013_SEX_AND_AGE__Median_age_y, A._2014_SEX_AND_AGE__Median_age_y, A._2015_SEX_AND_AGE__Median_age_y, A._2016_SEX_AND_AGE__Median_age_y,
+ B._2017_sex_and_age__total_population__median_age_, B._2018_sex_and_age__total_population__median_age_, B._2019_sex_and_age__total_population__median_age_,
+ B._2021_sex_and_age__total_population__median_age_
+ from acs_1_demographic_city_2016 A
+ join acs_1_demographic_city_2021 B on A.name = B.name
+ 
+ select A.name, A.state, A.county, A.tract, A."_sex_and_age__median_age_y", B."_sex_and_age__total_population__median_age_y"
+ from acs_5_demographic_census_tract_fl_2016 A
+ join acs_5_demographic_census_tract_fl_2021 B on A.name = B.name
+ 
+ select A.name, A.state, A.county, A.tract, 
+ round(CAST((((A."_sex_and_age__20_to_24_y" + A."_sex_and_age__25_to_34_y" + A."_sex_and_age__35_to_44_y") / A."_sex_and_age__total_population")*100) as numeric),2) as young_ratio_2016,
+ round(CAST((((B."_sex_and_age__total_population__20_to_24_y" + B."_sex_and_age__total_population__25_to_34_y" + B."_sex_and_age__total_population__35_to_44_y") / B."_sex_and_age__total_population")*100) as numeric),2) as young_ratio_2021
+ from acs_5_demographic_census_tract_fl_2016 A
+ join acs_5_demographic_census_tract_fl_2021 B on A.name = B.name
+ where A._sex_and_age__total_population != 0
 
--- median income
--- select A.Name, A.state, A.city, round(A.CP03_2013_062E *1.15) as _2013_median_household_income_adj, round(A.CP03_2014_062E *1.15) _2014_median_household_income_adj, round(A.CP03_2015_062E *1.15) _2015_median_household_income_adj, 
--- round(A.CP03_2016_062E*1.15) _2016_median_household_income_adj, B.CP03_2017_062E _2017_median_household_income_adj, B.CP03_2018_062E _2018_median_household_income_adj, 
--- B.CP03_2019_062E _2019_median_household_income_adj, B.CP03_2021_062E _2021_median_household_income_adj
--- from acs_1_economic_city_2016 A
--- join acs_1_economic_city_2021 B on A.name = B.name
 
 -- vacancy rate
 -- select A.Name, A.state, A.city, 'housing vacancy' as vacancy_type, A._2013_Homeowner_vacancy_rate as _2013_Rental_vacancy_rate,
@@ -234,6 +302,8 @@ where county = '031'
 -- from acs_1_housing_city_2016 A
 -- join acs_1_housing_city_2021 B on A.name = B.name
 
+ 
+ 
 -- renter-occupied
 -- select A.Name, A.state, A.city, A._2013_Occupied_housing_units__Renter_occupied,
 -- A._2014_Occupied_housing_units__Renter_occupied,
